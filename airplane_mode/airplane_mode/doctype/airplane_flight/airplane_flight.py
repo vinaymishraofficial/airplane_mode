32c0133c5b27e02cc2330fbcaacb32c6a0ad040e
vinaymishraofficial <vinay.alwar89@gmail.com>
2025-10-01 18:53:26 +0530

    Fix:  Try Patches

---
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2025, Vinay Mishra and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class AirplaneFlight(Document):
+	pass
+	def on_submit(self):
+		self.set_value("status", "Completed")
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2025, Vinay Mishra and contributors
 # For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
+import random
 
 class AirplaneTicket(Document):
 
@@ -14,18 +14,24 @@ class AirplaneTicket(Document):
     def remove_duplicate_addons(self):
         seen = set()                                                        # added a blank set here
         unique_rows = []                                                    # to store unique rows
-        for row in self.add_ons:                                             # iterate through child table rows
-            if row.item not in seen:                                           # check if item is already in set
+        for row in self.add_ons:                                            # iterate through child table rows
+            if row.item not in seen:                                        # check if item is already in set
                 seen.add(row.item)                                          # if not, add to set
                 unique_rows.append(row)                                     # and to unique rows list
-        # after loop ends, we have only unique rows in unique_rows list
-        # replace with only unique rows
+         # after loop ends, we have only unique rows in unique_rows list
+         # replace with only unique rows
         self.set("add_ons", unique_rows)
 
     def calculate_total_amount(self):
         """Total Amount = Flight Price + sum of all add-on amounts"""
-        child_total = sum([row.amount for row in self.add_ons])
-        self.total_amount = (self.flight_price or 0) + child_total
+        child_row_total_amount = sum([row.amount for row in self.add_ons])
+        self.total_amount = (self.flight_price or 0) + child_row_total_amount
+
+    def before_insert(self):
+        # Generate random seat number
+        number = random.randint(1, 99)  # Random number between 1 and 99
+        letter = random.choice(['A', 'B', 'C', 'D', 'E'])  # Random letter
+        self.seat = f"{number}{letter}"
 
     def on_submit(self):
         if self.status != "Boarded":
